c4d9cf9f53b2909eebac893315d8a368eac91d8a
Iced0368 <iced03682@gmail.com>
2024-07-20 13:53:57 +0900

    added return value cache

--- app.py
+++ app.py
@@ -34,6 +34,165 @@ if st.session_state['generated']:
 else:
     _, factors, _ = st.columns([1, 2, 1])
 
+@st.cache_data
+def dolggagggi(goal_level, hexa_frag_cost, hexa_prob, sunday_maple, frag_price, reset_cost, high_value_preference, num_frags, mode):
+    dol_result = {}
+
+    hsm = HexaStrategyModel(
+        goal_level, hexa_frag_cost, hexa_prob[sunday_maple], 
+        frag_price, reset_cost, 
+        MAX_LEVEL, MAX_ATTEMPTS, ITERATIONS, 
+        high_value_preference
+    )
+
+    ######## Apply Strategy ########
+
+    # 조각 소모
+    if mode:
+        dol_result['num_frags'] = num_frags
+        table = hexa_table_frag[sunday_maple]
+        strategy_table = copy.deepcopy(table)
+
+        x = {(0, 0, 0): 1} # main-level, attempts, spent-frags
+        init_states = []
+        for fsstate, prob in strategy_table.transitions.items():
+            from_state, to_state = fsstate
+            m, t, f = to_state
+            if hsm.should_stop[m][t] and f == 0:
+                init_states.append((from_state, to_state, prob))
+
+        for from_state, to_state, prob in init_states:
+            del strategy_table.transitions[(from_state, to_state)]
+            prev_prob = strategy_table.transitions.get((from_state, (0, 0, 0)), 0)
+            strategy_table.add_transition(from_state, (0, 0, 0), prev_prob + prob)
+
+
+        if high_value_preference:
+            for level in range(goal_level, MAX_LEVEL+1):
+                strategy_table.clear_transition((level, MAX_ATTEMPTS, 0))
+                strategy_table.add_transition((level, MAX_ATTEMPTS, 0), (level, MAX_ATTEMPTS, 0), 1.0)   
+        else:
+            for level in range(0, goal_level+1):
+                strategy_table.clear_transition((level, MAX_ATTEMPTS, 0))
+                strategy_table.add_transition((level, MAX_ATTEMPTS, 0), (level, MAX_ATTEMPTS, 0), 1.0)
+
+        strategy_table.compile()
+
+        ######## Do Reinforce ########
+
+        result_vector = np.zeros(MAX_LEVEL+1)
+
+        if high_value_preference:
+            result = reinforce(strategy_table, x, num_frags // hex_frag_multiplier)
+
+            for i in range(MAX_LEVEL+1):
+                result_vector[i] = result[(i,)]
+
+        else:
+            result = reinforce(strategy_table, x, num_frags // hex_frag_multiplier, False)
+            result = strategy_table.compress_state_dict(strategy_table.vector_to_state_dict(result), [0, 1])
+            for i in range(MAX_LEVEL+1):
+                result_vector[i] = result.get((i, MAX_ATTEMPTS), 0)
+
+        dol_result['result_vector'] = result_vector
+
+        success = np.sum(result_vector[goal_level:]) if high_value_preference else np.sum(result_vector[:goal_level+1])
+        dol_result['success'] = success
+
+    # 조각 무한 멸망전
+    else:
+        table = hexa_table[sunday_maple]
+        strategy_table = copy.deepcopy(table)
+
+        x = {(0, 0): 1}  # main-level, attempts
+
+        for m in range(MAX_LEVEL+1):
+            for t in range(MAX_ATTEMPTS+1):
+                if hsm.should_stop[m][t]:
+                    strategy_table.clear_transition((m, t))
+                    strategy_table.add_transition((m, t), (0, 0), 1.0)
+
+        if high_value_preference:
+            for level in range(goal_level, MAX_LEVEL+1):
+                strategy_table.clear_transition((level, MAX_ATTEMPTS))
+                strategy_table.add_transition((level, MAX_ATTEMPTS), (level, MAX_ATTEMPTS), 1.0)
+        else:
+            for level in range(0, goal_level+1):
+                strategy_table.clear_transition((level, MAX_ATTEMPTS))
+                strategy_table.add_transition((level, MAX_ATTEMPTS), (level, MAX_ATTEMPTS), 1.0)
+
+        strategy_table.compile()
+
+        ######## Calculate Expectation ########
+
+        def cost_branch(state, hsm, only_frag=False):
+            i, j = state
+            if hsm.should_stop[i][j]:
+                return reset_cost if not only_frag else 0
+            elif j == 20:
+                return 0
+            else:
+                return hexa_frag_cost[i] * hex_frag_multiplier * frag_price if not only_frag else hexa_frag_cost[i] * hex_frag_multiplier
+
+        strategy_cost = strategy_table.create_vector({
+            (i, j): cost_branch((i, j), hsm) for i, j in strategy_table.states
+        })
+
+        strategy_frag_cost = strategy_table.create_vector({
+            (i, j): cost_branch((i, j), hsm, True) for i, j in strategy_table.states
+        })
+
+        xvector = strategy_table.create_vector(x)
+        migs = matrix_semi_inf_geometric_series(strategy_table.get_matrix())
+
+        expected_cost = xvector @ migs @ strategy_cost.T
+        expected_frag_cost = xvector @ migs @ strategy_frag_cost.T
+
+        dol_result['expected_cost'] = expected_cost
+        dol_result['expected_frag_cost'] = expected_frag_cost
+
+        ######## Do Reinforce ########
+
+        result = reinforce(strategy_table, x, INF)
+
+        result_vector = np.zeros(MAX_LEVEL+1)
+        for i in range(MAX_LEVEL+1):
+            result_vector[i] = result[(i,)]
+
+        dol_result['result_vector'] = result_vector
+
+    chart_data = pd.DataFrame({
+        '메인 레벨': range(MAX_LEVEL + 1),
+        '결과 값': result_vector
+    }).set_index('메인 레벨')
+
+    dol_result['chart_data'] = chart_data
+
+
+    last_one_indices = []
+    for col in range(hsm.should_stop.shape[1]):
+        rows_with_ones = np.where(hsm.should_stop[:, col] == 1)[0]
+        if rows_with_ones.size > 0:
+            last_one_indices.append(rows_with_ones[-1])
+        else:
+            last_one_indices.append(-1)  # 1이 없는 경우 -1로 표시
+
+    prev = -1
+    data = {'강화 횟수': [], '메인 스탯': []}
+    for i in range(10, MAX_ATTEMPTS+1):
+        if last_one_indices[i] != prev:
+            data['강화 횟수'].append(f'{i}회 강화')
+            data['메인 스탯'].append(f'메인스탯 {last_one_indices[i]} 이하면 초기화')
+        prev = last_one_indices[i]
+
+    df = pd.DataFrame(data)
+    dol_result['df'] = df
+
+    dol_result['generated'] = True
+
+    return dol_result
+
+
 # 왼쪽 열에 입력 폼 배치
 with factors:
     with st.container(border=True):
@@ -79,148 +238,11 @@ with factors:
             st.session_state['goal_level'] = goal_level
             st.session_state['frag_mode'] = st.session_state['frag_toggle_state']
 
-            hsm = HexaStrategyModel(
-                goal_level, hexa_frag_cost, hexa_prob[sunday_maple], 
-                frag_price, reset_cost, 
-                MAX_LEVEL, MAX_ATTEMPTS, ITERATIONS, 
-                high_value_preference
-            )
+            dol_result = dolggagggi(goal_level, hexa_frag_cost, hexa_prob, sunday_maple, frag_price, reset_cost, high_value_preference, num_frags, st.session_state['frag_mode'])
 
-            ######## Apply Strategy ########
+            for key, value in dol_result.items():
+                st.session_state[key] = value
 
-            # 조각 소모
-            if st.session_state['frag_mode']:
-                st.session_state['num_frags'] = num_frags
-                table = hexa_table_frag[sunday_maple]
-                strategy_table = copy.deepcopy(table)
-
-                x = {(0, 0, 0): 1} # main-level, attempts, spent-frags
-                init_states = []
-                for fsstate, prob in strategy_table.transitions.items():
-                    from_state, to_state = fsstate
-                    m, t, f = to_state
-                    if hsm.should_stop[m][t] and f == 0:
-                        init_states.append((from_state, to_state, prob))
-
-                for from_state, to_state, prob in init_states:
-                    del strategy_table.transitions[(from_state, to_state)]
-                    prev_prob = strategy_table.transitions.get((from_state, (0, 0, 0)), 0)
-                    strategy_table.add_transition(from_state, (0, 0, 0), prev_prob + prob)
-
-
-                if high_value_preference:
-                    for level in range(goal_level, MAX_LEVEL+1):
-                        strategy_table.clear_transition((level, MAX_ATTEMPTS, 0))
-                        strategy_table.add_transition((level, MAX_ATTEMPTS, 0), (level, MAX_ATTEMPTS, 0), 1.0)   
-                else:
-                    for level in range(0, goal_level+1):
-                        strategy_table.clear_transition((level, MAX_ATTEMPTS, 0))
-                        strategy_table.add_transition((level, MAX_ATTEMPTS, 0), (level, MAX_ATTEMPTS, 0), 1.0)
-
-                strategy_table.compile()
-
-                ######## Do Reinforce ########
-
-                result_vector = np.zeros(MAX_LEVEL+1)
-
-                if high_value_preference:
-                    result = reinforce(strategy_table, x, num_frags // hex_frag_multiplier)
-
-                    for i in range(MAX_LEVEL+1):
-                        result_vector[i] = result[(i,)]
-
-                else:
-                    result = reinforce(strategy_table, x, num_frags // hex_frag_multiplier, False)
-                    result = strategy_table.compress_state_dict(strategy_table.vector_to_state_dict(result), [0, 1])
-                    for i in range(MAX_LEVEL+1):
-                        result_vector[i] = result.get((i, MAX_ATTEMPTS), 0)
-
-                st.session_state['result_vector'] = result_vector
-
-            # 조각 무한 멸망전
-            else:
-                table = hexa_table[sunday_maple]
-                strategy_table = copy.deepcopy(table)
-
-                x = {(0, 0): 1}  # main-level, attempts
-
-                for m in range(MAX_LEVEL+1):
-                    for t in range(MAX_ATTEMPTS+1):
-                        if hsm.should_stop[m][t]:
-                            strategy_table.clear_transition((m, t))
-                            strategy_table.add_transition((m, t), (0, 0), 1.0)
-
-                if high_value_preference:
-                    for level in range(goal_level, MAX_LEVEL+1):
-                        strategy_table.clear_transition((level, MAX_ATTEMPTS))
-                        strategy_table.add_transition((level, MAX_ATTEMPTS), (level, MAX_ATTEMPTS), 1.0)
-                else:
-                    for level in range(0, goal_level+1):
-                        strategy_table.clear_transition((level, MAX_ATTEMPTS))
-                        strategy_table.add_transition((level, MAX_ATTEMPTS), (level, MAX_ATTEMPTS), 1.0)
-
-                strategy_table.compile()
-
-                ######## Calculate Expectation ########
-
-                def cost_branch(state, hsm, only_frag=False):
-                    i, j = state
-                    if hsm.should_stop[i][j]:
-                        return reset_cost if not only_frag else 0
-                    elif j == 20:
-                        return 0
-                    else:
-                        return hexa_frag_cost[i] * hex_frag_multiplier * frag_price if not only_frag else hexa_frag_cost[i] * hex_frag_multiplier
-
-                strategy_cost = strategy_table.create_vector({
-                    (i, j): cost_branch((i, j), hsm) for i, j in strategy_table.states
-                })
-
-                strategy_frag_cost = strategy_table.create_vector({
-                    (i, j): cost_branch((i, j), hsm, True) for i, j in strategy_table.states
-                })
-
-                xvector = strategy_table.create_vector(x)
-                migs = matrix_semi_inf_geometric_series(strategy_table.get_matrix())
-
-                expected_cost = xvector @ migs @ strategy_cost.T
-                expected_frag_cost = xvector @ migs @ strategy_frag_cost.T
-
-                st.session_state['expected_cost'] = expected_cost
-                st.session_state['expected_frag_cost'] = expected_frag_cost
-
-                ######## Do Reinforce ########
-
-                result = reinforce(strategy_table, x, INF)
-
-                result_vector = np.zeros(MAX_LEVEL+1)
-                for i in range(MAX_LEVEL+1):
-                    result_vector[i] = result[(i,)]
-
-                st.session_state['result_vector'] = result_vector
-
-            ######## Show Strategy ########
-
-            last_one_indices = []
-            for col in range(hsm.should_stop.shape[1]):
-                rows_with_ones = np.where(hsm.should_stop[:, col] == 1)[0]
-                if rows_with_ones.size > 0:
-                    last_one_indices.append(rows_with_ones[-1])
-                else:
-                    last_one_indices.append(-1)  # 1이 없는 경우 -1로 표시
-
-            prev = -1
-            data = {'강화 횟수': [], '메인 스탯': []}
-            for i in range(10, MAX_ATTEMPTS+1):
-                if last_one_indices[i] != prev:
-                    data['강화 횟수'].append(f'{i}회 강화')
-                    data['메인 스탯'].append(f'메인스탯 {last_one_indices[i]} 이하면 초기화')
-                prev = last_one_indices[i]
-
-            df = pd.DataFrame(data)
-            st.session_state['df'] = df
-
-            st.session_state['generated'] = True
             st.rerun()
 
 if st.session_state['generated']:
@@ -241,15 +263,11 @@ if st.session_state['generated']:
 
                 elif st.session_state['frag_mode'] and 'num_frags' in st.session_state:
                     num_frags = st.session_state['num_frags'] 
-                    success = np.sum(result_vector[goal_level:]) if high_value_preference else np.sum(result_vector[:goal_level+1])
+                    success = st.session_state['success']
                     st.write(f'### 메인스탯 {goal_level} {"이상" if high_value_preference else "이하"} 도전 - 조각 {num_frags}개 달성률: {round(success * 100, 2)}%')
 
                 # result_vector를 st.bar_chart로 시각화
-                chart_data = pd.DataFrame({
-                    '메인 레벨': range(MAX_LEVEL + 1),
-                    '결과 값': result_vector
-                }).set_index('메인 레벨')
-                st.bar_chart(chart_data)
+                st.bar_chart(st.session_state['chart_data'])
 
                 st.write("### 전략 생성 결과")
                 st.table(st.session_state['df'])
